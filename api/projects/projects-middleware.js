1d3fbae8eb40104220753a60ebe835e69281ff2d
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-12-02 19:45:50 -0500

    middleware complete

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,39 @@
-// add middlewares here related to projects
+const Projects = require('./projects-model');
+
+function logger(req, res, next) {
+  const reqMethod = req.method;
+  const reqUrl = req.originalUrl;
+  const timestamp = new Date().toLocaleString();
+
+  console.log(`This is a ${reqMethod} request for ${reqUrl} at ${timestamp}`);
+  next();
+}
+
+async function validateProjectId(req, res, next) {
+  try {
+    const project = await Projects.get(req.params.id);
+    if (project) {
+      req.project = project;
+      next();
+    } else {
+      res.status(404).json({ message: 'Invalid project ID' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to get project' });
+  }
+}
+
+function validateProject(req, res, next) {
+  const { name, description } = req.body;
+  if (!name || !description) {
+    res.status(400).json({ message: 'Missing required fields' });
+  } else {
+    next();
+  }
+}
+
+module.exports = {
+  logger,
+  validateProjectId,
+  validateProject,
+};
--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { reset } = require('nodemon');
+const { logger } = require('../api/projects/projects-middleware');
+
 const server = express();
 
 server.use(express.json());
+server.use(logger);
 
 server.get('/', (req, res) => {
-  reset.send(`<h2>Build a Web API Sprint Challenge</h2>`);
+  res.send(`<h2>Build a Web API Sprint Challenge</h2>`);
 });
 
 module.exports = server;
