a9db2de624544026c6a3aacc9673248b803f904f
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-12-02 19:10:50 -0500

    server initial setup

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { reset } = require('nodemon');
 const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  reset.send(`<h2>Build a Web API Sprint Challenge</h2>`);
+});
 
 module.exports = server;
